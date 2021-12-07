f612f049a6fb2fa3dedff019fb07245cc3beeb95
priyaprecious <priyanka.datti@gmail.com>
2021-12-07 03:20:13 +0000

    installed_body_parser added get and post route for urls/new

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -35,9 +38,18 @@ app.get("/u/:shortURL", (req, res) => {
      }
   });
 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
     res.render("urls_show", templateVars);
+  });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
   });
 
 app.listen(PORT, () => {
